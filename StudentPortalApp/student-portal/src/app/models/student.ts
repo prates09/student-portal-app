48c380e8fc8df8351819aea24f4b9dab6b5ad24f
Pratyush <157958382+prates09@users.noreply.github.com>
2025-12-12 10:30:11 +0530

    Delete StudentPortalApp directory

--- StudentPortalApp/student-portal/src/app/models/student.ts
+++
@@ -1,8 +0,0 @@
-export interface Student {
-  name: string;
-  class: string;            // "Class 6", "Class 7", "Class 8", "Class 9"
-  gender: 'Male' | 'Female';
-  hasHobby: boolean;
-  hobby?: string;
-  favoriteSubject?: string;
-}
